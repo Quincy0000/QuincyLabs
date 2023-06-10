d342575524614a6c4115da0328ffa5696cb6df46
Quincy Cherrelus <132164351+Quincy0000@users.noreply.github.com>
2023-06-09 23:53:14 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,60 +1,18 @@
 import time
+time.sleep(0.1) # Wait for USB to become ready
 
-class TrafficLight:
-    def __init__(self):
-        self.state = "red"
-        self.timer = 0
+print("Hello, Pi Pico!")
 
-    def start_timer(self, duration):
-        self.timer = duration
-        while self.timer > 0:
-            print("Traffic light:", self.state)
-            time.sleep(1)
-            self.timer -= 1
 
-    def set_state(self, state):
-        self.state = state
+from PedestrianCrossing import PedestrianCrossing
+from TrafficLightController import Light
+from vehicle import VehicleDetectionHandler
 
-class PedestrianLight:
-    def __init__(self):
-        self.state = "don't walk"
-        self.timer = 0
+if _name_ == "_main_":
+    crossing = PedestrianCrossing()
+    crossing.run()
 
-    def start_timer(self, duration):
-        self.timer = duration
-        while self.timer > 0:
-            print("Pedestrian light:", self.state)
-            time.sleep(1)
-            self.timer -= 1
+    traffic_light = TrafficLightController()
+    traffic_light.control_traffic_lights()
 
-    def set_state(self, state):
-        self.state = state
-
-class TrafficLightController:
-    def __init__(self):
-        self.traffic_light = TrafficLight()
-        self.pedestrian_light = PedestrianLight()
-
-    def run(self):
-        while True:
-
-            self.traffic_light.set_state("red")
-            self.traffic_light.start_timer(3)
-
-            self.pedestrian_light.set_state("walk")
-            self.pedestrian_light.start_timer(2)
-
-            self.pedestrian_light.set_state("don't walk")
-            self.pedestrian_light.start_timer(2)
-
-            self.traffic_light.set_state("green")
-            self.traffic_light.start_timer(5)
-
-            self.traffic_light.set_state("yellow")
-            self.traffic_light.start_timer(2)
-
-# Create an instance of TrafficLightController
-controller = TrafficLightController()
-
-# Run the traffic light sequence
-controller.run()
+    vehicle_handler = VehicleDetectionHandler()
